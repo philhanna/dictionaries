acd88fe2384cc90bfbfad5b9475aab6129f452d8
Phil Hanna <ph1204@gmail.com>
2023-02-25 11:59:02 -0500

    Write map

--- main.go
+++ main.go
@@ -8,39 +8,65 @@ import (
 )
 
 func main() {
+
+	// Create a map of words to their counts
+	wordMap := make(map[string]int)
+
+	// Make lists of apostrophe words and what to do about them.
+	squashMe := []string{"'s", "'t"}
+	deleteMe := []string{"'ll", "'m"}
+	cutMe := []string{"'"}
+	
+	// Form the regular expression
+	re := regexp.MustCompile(`\b[\w']+\b`)
+
+	// Read the whole file as a string
 	text, _ := os.ReadFile("testdata/tolstoy/6157-0.txt")
 	stext := string(text)
 
-	fp, err := os.Create("/tmp/tolstoy.txt")
-	defer fp.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	
-	re := regexp.MustCompile(`\b[\w']+\b`)
+	// Read the text and extract the words
 	for _, word := range re.FindAllString(stext, -1) {
-		
-		squashme := []string{"'s", "'t"}
-		for _, suffix := range squashme {
+		for _, suffix := range squashMe {
 			if strings.HasSuffix(word, suffix) {
 				word = word[:len(word)-len(suffix)]
 				word = word + suffix[1:]
 			}
-		}
-		
-		deleteme := []string{"'ll", "'m"}
-		for _, suffix := range deleteme {
+		}		
+		for _, suffix := range deleteMe {
 			if strings.HasSuffix(word, suffix) {
 				word, _ = strings.CutSuffix(word, suffix)
 			}
 		}
-
-		p := strings.Index(word, "'")
-		if p != -1 {
-			word = word[:p]
+		for _, suffix := range cutMe {
+			p := strings.Index(word, suffix)
+			if p != -1 {
+				word = word[:p]
+			}
 		}
 
-		fmt.Fprintln(fp, word)
+		// Convert to uppercase
+		word = strings.ToUpper(word)
+
+		// Add to map
+		_, ok := wordMap[word]
+		if ok {
+			wordMap[word]++
+		} else {
+			wordMap[word] = 1
+		}
 	}
+
+	// Open the output file
+	fpout, err := os.Create("/tmp/tolstoy.txt")
+	defer fpout.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Write the map
+	for word, count := range wordMap {
+		fmt.Fprintf(fpout, "%s,%d\n", word, count)
+	}
+
 }
